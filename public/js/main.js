a7b21d94cc96393eb1ea1ddb3b05282e7b9bafad
mostafa.rashidi <mostafa.rashidinasab@gmail.com>
2023-11-16 08:29:21 +0330

    battlenet-part2

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,14 @@
+import Header from "../module/header";
+import navbar from "../module/navbar";
+import slider1 from "../module/slider1";
+import product from "../module/product";
+import footer from "../module/footer";
+Header();
+navbar();
+slider1();
+product();
+footer();
+
 // slider-1
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
@@ -18,4 +29,3 @@ var swiper = new Swiper(".weSwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
